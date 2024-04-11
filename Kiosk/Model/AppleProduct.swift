47c4092e725170a93c5f67c7135a15cb4c42564d
Haroldfromk <dongik369@naver.com>
2024-04-11 15:52:05 +0900

    Initial Commit

---
+++ Kiosk/Model/AppleProduct.swift
@@ -0,0 +1,18 @@
+//
+//  AppleProduct.swift
+//  Kiosk
+//
+//  Created by imhs on 4/3/24.
+//
+
+import Foundation
+import UIKit
+
+struct AppleProduct {
+    let image: UIImage?
+    let name: String
+    let price: Int
+    let category: String
+    var value: Int 
+    var isNew: Bool
+}
